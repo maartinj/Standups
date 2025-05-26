747630332afff866c9b17d6ac8e6a4009a3c141d
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2025-05-26 11:16:43 +0200

    Initial Commit

---
+++ Standups/StandupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StandupsApp.swift
+//  Standups
+//
+//  Created by Marcin Jędrzejak on 26/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
