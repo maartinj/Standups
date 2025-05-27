45740a9454e79191be3f90a54e58f90d80595c1c
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2025-05-27 11:42:01 +0200

    feat: Viewing and editing a standup (completed Navigation + added missing colors)

---
+++ StandupsTests/StandupDetailTests.swift
@@ -0,0 +1,34 @@
+//
+//  StandupDetailTests.swift
+//  CounterTests
+//
+//  Created by Marcin Jędrzejak on 27/05/2025.
+//
+
+import ComposableArchitecture
+import XCTest
+@testable import Standups
+
+@MainActor
+final class StandupDetailTests: XCTestCase {
+    func testEdit() async throws {
+        var standup = Standup.mock
+        let store = TestStore(
+            initialState: StandupDetailFeature.State(
+                standup: standup
+            )
+        ) {
+            StandupDetailFeature()
+        }
+        store.exhaustivity = .off
+
+        await store.send(.editButtonTapped)
+        standup.title = "Point-Free Morning Sync"
+        await store.send(
+            .editStandup(.presented(.set(\.$standup, standup)))
+        )
+        await store.send(.saveStandupButtonTapped) {
+            $0.standup.title = "Point-Free Morning Sync"
+        }
+    }
+}
