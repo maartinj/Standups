0707cc06b2338e7686dd26563a5de57fef5a99b0
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2025-05-27 02:31:15 +0200

    feat: Adding a standup

--- Standups/StandupsApp.swift
+++ Standups/StandupsApp.swift
@@ -5,13 +5,26 @@
 //  Created by Marcin Jędrzejak on 26/05/2025.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct StandupsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                StandupsListView(
+                    store: Store(initialState: StandupsListFeature.State(
+                        addStandup: StandupFormFeature.State(
+                            focus: .attendee(Standup.mock.attendees[2].id),
+                            standup: .mock
+                        )
+                    )
+                    ) {
+                        StandupsListFeature()
+                    }
+                )
+            }
         }
     }
 }
